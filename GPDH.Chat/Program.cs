0408a3800cdc029833cc7e354aff9a16896f3cec
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-11-22 20:30:20 +0700

    first commit

--- GPDH.Chat/Program.cs
+++ GPDH.Chat/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,26 @@ namespace GPDH.Chat
     {
         static void Main(string[] args)
         {
+            var hostName = Dns.GetHostName();
+            var ipAddresses = Dns.GetHostAddresses(hostName);
+            var hostIpAddress = ipAddresses.FirstOrDefault(ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork);
+            var ipEndPoint = new IPEndPoint(hostIpAddress, 12345);
+            var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ipEndPoint);
+            server.Listen(10);
+            Console.WriteLine("Server Started");
+            while (true)
+            {
+                var messageHandler = server.Accept();
+                var messageBytes = new byte[1024];
+                var numberOfBytes = messageHandler.Receive(messageBytes);
+                var message = Encoding.Unicode.GetString(messageBytes, 0, numberOfBytes);
+                Console.OutputEncoding = Encoding.Unicode;
+                Console.WriteLine(message);
+                messageHandler.Shutdown(SocketShutdown.Both);
+                messageHandler.Close();
+            }
+            
         }
     }
 }
---
+++ GPDH.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GPDH.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hostName = Dns.GetHostName();
+            var ipAddresses = Dns.GetHostAddresses(hostName);
+            var hostIpAddress = ipAddresses.FirstOrDefault(ipAddress => ipAddress.AddressFamily == AddressFamily.InterNetwork);
+            var remoteEndPoint = new IPEndPoint(hostIpAddress, 12345);
+            var sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(remoteEndPoint);
+            Console.WriteLine("Connected to {0}", remoteEndPoint);
+            var message = Encoding.Unicode.GetBytes("Xin chào bạn");
+            sender.Send(message);
+            Console.ReadKey();
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+    }
+}
